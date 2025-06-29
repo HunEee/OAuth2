5097489add585c36d9eac8019103e3304ad66f72
HunEee <asdf981001@naver.com>
2025-06-29 13:09:16 +0900

    OAuth2 네이버 로그인 구현

--- src/main/java/com/spring_security/zAuthSession/config/SecurityConfig.java
+++ src/main/java/com/spring_security/zAuthSession/config/SecurityConfig.java
@@ -7,10 +7,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.spring_security.zAuthSession.service.CustomOAuth2UserService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
+	
+	
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -18,7 +27,11 @@ public class SecurityConfig {
         	.formLogin((login) -> login.disable()) // 폼로그인 사용X
         	.httpBasic((basic) -> basic.disable()); // http basic 방식 사용X
 
-        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Login(
+        		(oauth2) -> oauth2.userInfoEndpoint(
+							  (userInfoEndpointConfig) -> userInfoEndpointConfig.userService(customOAuth2UserService)
+                        	)
+		);
 
         //경로에 대한 인가 작업
         http.authorizeHttpRequests((auth) -> auth
