bce6e55da65083b81e46456d8e8db3ccff5cae34
HunEee <asdf981001@naver.com>
2025-07-01 22:56:04 +0900

    커스텀 로그인 구현

--- src/main/java/com/spring_security/zAuthSession/config/SecurityConfig.java
+++ src/main/java/com/spring_security/zAuthSession/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.spring_security.zAuthSession.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,25 +18,26 @@ public class SecurityConfig {
         this.customOAuth2UserService = customOAuth2UserService;
     }
 	
-	
+   
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf((csrf) -> csrf.disable()) // 개발 환경에서 꺼줌
-        	.formLogin((login) -> login.disable()) // 폼로그인 사용X
+        	//.formLogin((login) -> login.disable()) // 폼로그인 사용X
         	.httpBasic((basic) -> basic.disable()); // http basic 방식 사용X
-
-        http.oauth2Login(
-        		(oauth2) -> oauth2.userInfoEndpoint(
-							  (userInfoEndpointConfig) -> userInfoEndpointConfig.userService(customOAuth2UserService)
-                        	)
-		);
 
         //경로에 대한 인가 작업
         http.authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/WEB-INF/jsp/**", "/", "/oauth2/**", "/login/**").permitAll()
+                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
                         .anyRequest().authenticated()
         );
+        
+        //커스텀 로그인
+        http.oauth2Login(
+        		(oauth2) -> oauth2.loginPage("/login")
+                				  .userInfoEndpoint((userInfoEndpointConfig) ->
+                        userInfoEndpointConfig.userService(customOAuth2UserService))
+		);
 
         return http.build();
     }
